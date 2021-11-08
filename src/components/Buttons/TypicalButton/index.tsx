1941cfbf1f932d811e47272ffaada6ba261579ae
Alex Pavlov <halfer.orc@gmail.com>
2021-11-08 23:23:29 +0300

    fix button props

--- src/components/Buttons/TypicalButton/index.tsx
+++ src/components/Buttons/TypicalButton/index.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import {Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 
 const TypicalButton: React.FC<{
   action: () => void;
   text: string;
-  buttonStyles: {} | {}[];
-  textStyles: {} | {}[];
+  buttonStyles?: {} | {}[];
+  textStyles?: {} | {}[];
 }> = ({action, text, buttonStyles, textStyles}) => {
   return (
-    <TouchableOpacity onPress={action} style={buttonStyles}>
-      <Text style={textStyles}>{text}</Text>
+    <TouchableOpacity onPress={action} style={buttonStyles || styles.button}>
+      <Text style={textStyles || styles.buttonText}>{text}</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#515ee9',
+    paddingVertical: 6,
+    paddingHorizontal: 10,
+    minWidth: 100,
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 export default TypicalButton;
