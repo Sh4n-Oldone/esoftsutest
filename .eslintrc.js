010e261b6ce5ec5b1cdba4705717215b97294e53
Alex Pavlov <halfer.orc@gmail.com>
2021-11-10 21:34:24 +0300

    fix project style and add test lib

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
+    'jsx-quotes': ['error', 'prefer-single'],
+  },
 };
